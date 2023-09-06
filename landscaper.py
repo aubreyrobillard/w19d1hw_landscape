7c798bff031a594e9ced13bd4d6ebe68d042e0b9
Aubrey Robillard <aubreyrobillard@Aubreys-MacBook-Pro.local>
2023-09-05 23:30:35 -0500

    landscaper can purchase scissors and use them to cut the grass

--- landscaper.py
+++ landscaper.py
@@ -5,16 +5,68 @@ game_data= {
 }
 
 while(True):
-    user_input = int(input("""
+    if(game_data['money'] < 5):
+        user_input = int(input("""
                         would you like to...
                         [1] Use your teeth to cut the grass and earn $1?
                         [2] Quit the game?
                     """))
+        if(user_input == 1):
+            game_data["money"] += 1
+            print(f"you earned $1")
+            print(f"you have $ {game_data['money']} in your pocket")
+
+        if(user_input == 2):
+            game_data["quit"] = True
+
+        if(game_data["quit"] == True):
+            print(f"game over")
+            break
+
+    if(game_data['money'] >= 5):
+        user_input = int(input("""
+                            would you like to...
+                            [1] Use your teeth to cut the grass and earn $1?
+                            [2] Buy a pair of rusty scissors for $5?
+                            [3] Quit the game?
+                        """))
+        if(user_input == 1):
+            game_data["money"] += 1
+            print(f"you earned $1")
+            print(f"you have $ {game_data['money']} in your pocket")
+
+        if(user_input == 2):
+            game_data['money'] -= 5
+            print(f"you purchased a pair of rusty scissors")
+            print(f"you have ${game_data['money']} in your pocket")
+            break
+
+        if(user_input == 3):
+            game_data["quit"] = True
+
+        if(game_data["quit"] == True):
+            print(f"game over")
+            break
+
+while(True):
+    user_input = int(input("""
+                            would you like to...
+                            [1] Use the rusty scissors to cut the grass for $5?
+                            [2] Use your teeth to cut the grass for $1?
+                            [3] Quit the game?
+                        """))
+
     if(user_input == 1):
-        game_data["money"] += 1
-        print(f"You have $ {game_data['money']} in your pocket")
+        game_data['money'] += 5
+        print(f"you earned $5")
+        print(f"you have $ {game_data['money']} in your pocket")
 
     if(user_input == 2):
+        game_data["money"] += 1
+        print(f"you earned $1")
+        print(f"You have $ {game_data['money']} in your pocket")
+
+    if(user_input == 3):
         game_data["quit"] = True
 
     if(game_data["quit"] == True):
